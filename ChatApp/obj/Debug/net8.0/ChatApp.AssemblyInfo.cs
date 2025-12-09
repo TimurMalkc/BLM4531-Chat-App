cc0f08acbc8d6dc444efb7d9700b17a4b6468b1f
Timur Malkoc <timurmlkc@gmail.com>
2025-12-09 23:29:12 +0300

    added user indicator

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+805f54730856205555694b7a494ee1cfd4b4aa49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf44cb51712ddfbccc2276f985f7634539936dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
